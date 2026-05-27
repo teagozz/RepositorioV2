0a6e3e5b3c30d085d5f9df339af0e28a6cab1c4a
Thiago Dias <thiago2020dias@gmail.comgit config --global user.email thiago2020dias@gmail.com>
2026-05-27 15:36:10 -0300

    Fix folder case sensitivity (css, javascript, img)

---
+++ javascript/script.js
@@ -0,0 +1,18 @@
+import { animate, stagger, splitText } from 'https://cdn.jsdelivr.net/npm/animejs@4.4.1/dist/bundles/anime.esm.js';
+
+const { chars } = splitText('.text-thiago', { words: false, chars: true });
+
+animate(chars, {
+  y: [
+    { to: '-2.75rem', ease: 'outExpo', duration: 800 },
+    { to: 0, ease: 'outBounce', duration: 800, delay: 100 }
+  ],
+  rotate: {
+    from: '-1turn',
+    delay: 0
+  },
+  delay: stagger(50),
+  ease: 'inOutCirc',
+  loopDelay: 1000,
+  loop: true
+});
